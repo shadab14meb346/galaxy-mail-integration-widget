7252691ec7683069d236d39f0d8298dd2d459f25
shadab <shadab14meb346@gmail.com>
2022-07-03 01:19:20 +0530

    add widget code

---
+++ widget-entry.js
@@ -0,0 +1,5 @@
+(function (document, script) {
+	script = document.createElement("script");
+	script.setAttribute("src", "widget-assets.js");
+	document.getElementsByTagName("head")[0].appendChild(script);
+})(document);
