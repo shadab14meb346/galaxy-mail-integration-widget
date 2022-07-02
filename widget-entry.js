adf215679ccb1cac16cf11b4eaae34031d391f2d
shadab <shadab14meb346@gmail.com>
2022-07-03 01:23:47 +0530

    host on cdn

--- widget-entry.js
+++ widget-entry.js
@@ -1,5 +1,8 @@
 (function (document, script) {
 	script = document.createElement("script");
-	script.setAttribute("src", "widget-assets.js");
+	script.setAttribute(
+		"src",
+		"https://cdn.jsdelivr.net/gh/shadab14meb346/galaxy-mail-integration-widget/widget-assets.js"
+	);
 	document.getElementsByTagName("head")[0].appendChild(script);
 })(document);
